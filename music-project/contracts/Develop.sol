3f7e012fc44473cbcbdf1ae3e48dd3b1a6568ce9
Adwaita Raj Modak <adwaitaraj@gmail.com>
2022-08-31 21:28:48 +0530

    Solidity updated

---
+++ music-project/contracts/Develop.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Develop{
+    address public owner = msg.sender;
+    uint public last_completed_migration;
+    
+    modifier restricted() {
+        require(
+            msg.sender == owner,
+            "This function is restricted to the contract's owner"
+        );
+        _;
+    }
+    
+    function setCompleted(uint completed) public restricted {
+        last_completed_migration = completed;
+    }
+
+    function getCompleted() public view returns (uint) {
+        return last_completed_migration;
+    }
+
+
+    
+}
